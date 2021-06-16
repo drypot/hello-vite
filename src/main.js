7883b26b845cecf1d44a3eda5e80f744313fcba3
drypot <drypot@gmail.com>
2021-06-16 17:52:14 +0900

    add tailwind support.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import './tailwind.css'
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1 class="text-2xl">Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
   <p>Version 0.0.3</p>
+  <div>
+    <button class="btn btn-green">Click</button>
+  </div>
 `
